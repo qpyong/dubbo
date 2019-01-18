dd6583112ecfeac0f2a2eb7c0a759e43665260c4
qpyong <qpy_2006@163.com>
2019-01-18 23:05:11 +0800

    重构商品服务接口实现

--- DubboDemo/src/main/java/org/qpyong/demo/dubbo/GoodsRemoteService.java
+++ DubboDemo/src/main/java/org/qpyong/demo/dubbo/GoodsRemoteService.java
@@ -1,30 +1,42 @@
 package org.qpyong.demo.dubbo;
 
 import org.qpyong.demo.dubbo.api.GoodsService;
+import org.qpyong.demo.model.Goods;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service("goodsService")
 public class GoodsRemoteService implements GoodsService {
-    Map<Long, String> names = new HashMap<Long, String>();
+    Map<Long, Goods> goodsMap = new HashMap<Long, Goods>();
 
     public GoodsRemoteService() {
-        names.put(1L, "No1");
-        names.put(2L, "No2");
-        names.put(3L, "No3");
+        for (int i = 0; i < 10; i++) {
+            goodsMap.put(new Integer(i).longValue(), createGoods(i));
+        }
     }
 
-    public List<String> getNames() {
-       List<String> list = new ArrayList<String>();
-       Iterator<String> it = names.values().iterator();
-       while(it.hasNext()){
-           list.add(it.next());
-       }
-       return list;
+    private Goods createGoods(long goodId) {
+        Goods goods = new Goods();
+        goods.setId(goodId);
+        goods.setName("爱他美奶粉" + goodId);
+        goods.setExpiryDate(24);
+        goods.setOpgCode("G00" + goodId);
+        goods.setProductDate(Calendar.getInstance().getTime());
+        return goods;
     }
 
-    public String getName(long id) {
-        return names.get(id);
+
+    public Goods getGoods(long l) {
+        return goodsMap.get(l);
+    }
+
+    public List<Goods> getAllGoods() {
+        Iterator<Goods> it = goodsMap.values().iterator();
+        List<Goods> list = new ArrayList<Goods>();
+        while (it.hasNext()) {
+            list.add(it.next());
+        }
+        return list;
     }
 }
